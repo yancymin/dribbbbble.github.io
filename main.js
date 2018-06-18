4233865e07e859c78eddc15779b90315a97df300
yancymin <senseslofter@gmail.com>
2018-06-18 17:58:03 +0800

    空白表单检测

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-var submit = document.getElementById("btn-submit");
-console.log(submit);
+
+
+// var submitBtn = document.getElementById("btn-submit");
+// var emailString = document.getElementById("form-email");
+// var child = document.getElementById("hidden");
+// var parent = document.getElementById("form-wrap")
+
+// submitBtn.addEventListener('click', function () {
+//     if (emailString.length > 0) {
+//         //提交表单
+//     } else {
+//         child.parentNode.removeChild(child);
+//         alert("Please enter your email");
+//     }
+// })
